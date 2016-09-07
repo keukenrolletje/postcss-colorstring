567d656ab6b1cbce3852b17813f711ca1733a9b0
Joke Gysen <jokegysen@gmail.com>
2016-09-07 09:13:23 +0200

    Remove hashtags

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ module.exports = postcss.plugin('postcss-celebcolors', function (opts) {
           var validChars = ['a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
         // Transform CSS AST here
+            /* If character is #, remove from array */
+            if(letter === '#'){
+              colorString.splice(letter, 1);
+            }
 
     };
 });
