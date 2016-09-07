9adf9ee18bc49f767adb8d3f43586d84c3361bbb
Joke Gysen <jokegysen@gmail.com>
2016-09-07 09:12:44 +0200

    Add valid characters

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports = postcss.plugin('postcss-celebcolors', function (opts) {
     // Work with options here
 
     return function (root, result) {
+          var validChars = ['a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
         // Transform CSS AST here
 
