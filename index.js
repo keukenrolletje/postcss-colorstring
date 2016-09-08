ce7b373e2662420146a8528ac4ab4504b71a5ba9
Joke Gysen <jokegysen@gmail.com>
2016-09-08 16:28:39 +0200

    Allow uppercase

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = postcss.plugin('postcss-celebcolors', function(opts) {
         css.walkDecls(decl => {
             var colorString = decl.value.split('');
             //uppercase should work!!
-            var validChars = ['a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+            var validChars = ['a', 'b', 'c', 'd', 'e', 'f', 'A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 
             function largeArr(arr) {
